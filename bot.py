b4d7be9ee14fdb822339609c153c3c0a05526fb2
vanillakernel <vanillakernel@gmail.com>
2015-12-23 11:28:18 -0800

    got word groups working with a generator I don't fully understand yet.

--- bot.py
+++ bot.py
@@ -4,36 +4,35 @@ from pprint import pprint
 import argparse
 import re
 def main ():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("file", help="A sample file to markov chain.")
-  args=parser.parse_args()
-  file = args.file
-  cohesion=2 # Number of words to match for keys.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="A sample file to markov chain.")
+    args=parser.parse_args()
+    file = args.file
+    cohesion=2 # Number of words to match for keys.
   
-  #read credentials from a file
-  with open('sample.txt') as sample_file:    
-
-    # If the authentication was successful, you should
-    # see the name of the account print out
-    sample_text=sample_file.read()
-    #word_dict=dict(x.split(' ') for x in sample_text.split('\n'))
-    delimiters = ['\n', ' ', ',', '.', '?', '!', ':', 'and_what_else_you_need']
+    #read credentials from a file
+    with open('sample.txt') as sample_file:    
+      sample_text=sample_file.read()
+  
     words = filter(None,( re.split('[-,.\"?! :\n]', sample_text)) ) 
-    pprint(words);
+  
+    triple_generator = Group(words)
+    for triple in triple_generator:
+      print(triple)
    
 
+# This will take the word array and make N-ples where the cohesion variable
+# dictates how many words are in the key. 
 def Group(word_array):
-  return
-  # This will take the word array and make N-ples where the cohesion variable
-  # dictates how many words are in the key. 
-
+    if len(word_array) < 3:
+	return						
+    for i in range(len(word_array) - 2):
+	yield (word_array[i], word_array[i+1], word_array[i+2])
+	
+#This will map the N-ples as "W1-Wn":"Wlast"
 def ToDict(tuple_array):
   return
-  #this will map the N-ples as "W1-Wn":"Wlast"
   
-
-
-
 if __name__ == "__main__":
   main()
 
